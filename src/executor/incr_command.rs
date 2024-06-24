f49ef43a59e11b4011a13dfd8c93da32d99e4142
gregor <e@tokarev.work>
2024-06-24 09:25:40 +0300

    codecrafters submit [skip ci]

--- src/executor/incr_command.rs
+++ src/executor/incr_command.rs
@@ -8,25 +8,33 @@ use crate::{
 use tokio::{io::AsyncWriteExt, net::TcpStream, time::Duration};
 
 pub async fn incr_command(socket: &mut TcpStream, context: &CommandContext, command: IncrCommand) {
+    let mut transaction = context.multi_exec.lock().await;
+    if transaction.active {
+        transaction.store_action(Box::new(())).await;
+        socket.write_all(b"+QUEUED\r\n").await.unwrap();
+        return;
+    };
+
     let mut storage = context.storage.lock().await;
 
     match storage.get(&command.key).await {
-        Some(val) => {
-            match val {
-                Item::Numeric(n) => {
-                    let value = Item::Numeric(n + command.step);
-                    storage.set(command.key, value.clone(), None).await;
+        Some(val) => match val {
+            Item::Numeric(n) => {
+                let value = Item::Numeric(n + command.step);
+                storage.set(command.key, value.clone(), None).await;
 
-                    socket
-                        .write(value.build_response_string().as_bytes())
-                        .await
-                        .unwrap();
-                }
-                _ => {
-                    socket.write_all(b"-ERR value is not an integer or out of range\r\n").await.unwrap();
-                }
+                socket
+                    .write(value.build_response_string().as_bytes())
+                    .await
+                    .unwrap();
             }
-        }
+            _ => {
+                socket
+                    .write_all(b"-ERR value is not an integer or out of range\r\n")
+                    .await
+                    .unwrap();
+            }
+        },
         None => {
             let value = Item::Numeric(command.step);
             storage.set(command.key, value.clone(), None).await;
