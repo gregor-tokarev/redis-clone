7f941793a9f4b9869caf8df766c56a65ab009529
gregor <e@tokarev.work>
2024-06-25 06:32:18 +0300

    codecrafters submit [skip ci]

--- src/executor/discard_command.rs
+++ src/executor/discard_command.rs
@@ -6,7 +6,7 @@ use tokio::{io::AsyncWriteExt, net::TcpStream};
 
 pub async fn discard_command(socket: &mut TcpStream, _context: &CommandContext, _command: Command, transaction: &mut TransactionContainer) {
     if !transaction.active {
-        socket.write_all(b"-ERR EXEC without MULTI\r\n").await.unwrap();
+        socket.write_all(b"-ERR DISCARD without MULTI\r\n").await.unwrap();
         return;
     }
 
